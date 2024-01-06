63a2592655b1124433e86f222ae9e7d031a949e3
unongmilkumk <unongmilkumk@gmail.com>
2024-01-06 17:12:29 +0900

    asdf

---
+++ src/main/java/com/unongmilk/Ability.java
@@ -0,0 +1,6 @@
+package com.unongmilk;
+
+interface Ability {
+    String name = "ERROR";
+    String description = "ERROR";
+}
--- src/main/java/com/unongmilk/Main.java
+++ src/main/java/com/unongmilk/Main.java
@@ -1,5 +1,19 @@
 package com.unongmilk;
 
-public class Main {
-    
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class Main extends JavaPlugin {
+    HashMap<UUID, Ability> ability = new HashMap<>();
+    @Override
+    public void onEnable() {
+        getLogger().info("Minecraft Ability War Enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Minecraft Ability War Disabled");
+    }
 }
